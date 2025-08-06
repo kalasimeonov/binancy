eef33f6ec7c0865c731e8ee610f926c73ec22304
Dimitar <dimitarvid@hotmail.com>
2025-08-06 13:39:52 +0300

    Initial Commit

---
+++ binancy/binancyApp.swift
@@ -0,0 +1,17 @@
+//
+//  binancyApp.swift
+//  binancy
+//
+//  Created by Dimitar Videnov on 6.08.25.
+//
+
+import SwiftUI
+
+@main
+struct binancyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ binancyTests/binancyTests.swift
@@ -0,0 +1,17 @@
+//
+//  binancyTests.swift
+//  binancyTests
+//
+//  Created by Dimitar Videnov on 6.08.25.
+//
+
+import Testing
+@testable import binancy
+
+struct binancyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
